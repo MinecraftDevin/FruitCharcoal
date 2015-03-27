f88fb1ad65441a39b8ce524037ada0cd728f08b8
MinecraftDevin <dsnell.snell@gmail.com>
2015-03-26 18:12:34 -0700

    Updated versions to BETA from ALPHA

--- src/main/java/com/minecraftdevin/fruitcharcoal/reference/Reference.java
+++ src/main/java/com/minecraftdevin/fruitcharcoal/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
     public static final String MOD_ID = "FruitCharcoal";
     public static final String MOD_NAME = "Fruit Charcoal";
-    public static final String VERSION = "1.7.10-0.2-Alpha";
+    public static final String VERSION = "1.7.10-1.0-beta";
 
 }
