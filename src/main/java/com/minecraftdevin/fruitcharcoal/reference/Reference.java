77def225685a62468ff8c6dc9cc526c086bbc15a
MinecraftDevin <dsnell.snell@gmail.com>
2015-05-13 21:43:54 -0700

    Added some more things to the build.gradle
    I always forget to fix versions too...

--- src/main/java/com/minecraftdevin/fruitcharcoal/reference/Reference.java
+++ src/main/java/com/minecraftdevin/fruitcharcoal/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "FruitCharcoal";
     public static final String MOD_NAME = "Fruit Charcoal";
-    public static final String VERSION = "1.7.10-1.1";
+    public static final String VERSION = "1.7.10-1.2";
     public static final String GUI_FACTORY_CLASS = "com.minecraftdevin.fruitcharcoal.client.gui.GuiFactory";
 
 }
